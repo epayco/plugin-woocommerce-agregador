d358bbf5e27391fba0b22f4e29c5c0c02fe788a8
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2022-05-04 10:25:29 -0500

    fix: se corrije valores de split para split con valores fijos

--- woocommerce-agregador-payco.php
+++ woocommerce-agregador-payco.php
@@ -657,11 +657,20 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
                     $epayco_p_cust_id_client = get_post_meta( $product["product_id"], 'p_cust_id_client_a' );
                     $receiversa['id'] = $epayco_p_cust_id_client[0];
                     $epayco_super_product = get_post_meta( $product["product_id"], '_super_product_a' );
-                    if($epayco_super_product[0] == "yes"){
-                        $receiversa['fee'] = floatval($product['total']);
+                    $epayco_epayco_comition = get_post_meta( $product["product_id"], 'epayco_comition_a' );
+
+                    if($epayco_super_product[0] != "yes"){
+                        $productTotalComision = floatval($epayco_epayco_comition[0])*$product["quantity"];
+                        $receiversa['total'] = floatval($product['total']) ;
+                        $fee = floatval($product['total'])-$productTotalComision;
+                        $receiversa['iva'] = 0;
+                        $receiversa['base_iva'] = 0;
+                        $receiversa['fee'] = $fee;
                     }else{
-                        $epayco_epayco_comition = get_post_meta( $product["product_id"], 'epayco_comition_a' );
-                        $receiversa['fee'] = floatval($epayco_epayco_comition[0]);
+                        $receiversa['total'] =  floatval($product['total']);
+                        $receiversa['iva'] = 0;
+                        $receiversa['base_iva'] = 0;
+                        $receiversa['fee'] = 0;
                     }
                     $clearData = str_replace('_', ' ', $this->string_sanitize($product['name']));
                     $descripcionParts[] = $clearData;
@@ -798,13 +807,13 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
                             if(split == "true"){
                                 var js_array ='.json_encode($receiversInfo).';
                                 let split_receivers = [];
-                                for(var jsa of js_array){
+                                 for(var jsa of js_array){
                                     split_receivers.push({
                                         "id" :  jsa.id,
-                                        "total": jsa.fee,
-                                        "iva" : 0,
-                                        "base_iva": 0,
-                                        "fee" : 0
+                                        "total": jsa.total,
+                                        "iva" : jsa.iva,
+                                        "base_iva": jsa.base_iva,
+                                        "fee" : jsa.fee
                                     });
                                 }
                                 data.split_app_id= "%s", //Id de la cuenta principal
