5d1c823a718255c50d8a58d23a000ca83ed3f3cb
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2022-08-02 11:24:52 -0500

    fix:se ajusta checkout apuntando a produccion

--- woocommerce-agregador-payco.php
+++ woocommerce-agregador-payco.php
@@ -783,9 +783,7 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
                             <img src="'.$epaycoButtonImage.'">
                          </a>
                         <form id="appAgregador">
-                            <script
-                                src="https://epayco-checkout-testing.s3.amazonaws.com/checkout.preprod.js?version=1643645084821"
-                                >
+                            <script src="https://checkout.epayco.co/checkout.js">
                             </script>
                             <script>
                             var handler = ePayco.checkout.configure({
@@ -968,7 +966,7 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
                         $ref_payco=$explode[1];
                     }
                     
-                    $url = 'https://secure.epayco.io/validation/v1/reference/'.$ref_payco;
+                    $url = 'https://secure.epayco.co/validation/v1/reference/'.$ref_payco;
                     $response = wp_remote_get(  $url );
                     $body = wp_remote_retrieve_body( $response );
                     $jsonData = @json_decode($body, true);
@@ -1451,7 +1449,7 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
             {
                 $username = sanitize_text_field($validationData['epayco_publickey']);
                 $password = sanitize_text_field($validationData['epayco_privatey']);
-                $response = wp_remote_post( 'https://apify.epayco.io/login', array(
+                $response = wp_remote_post( 'https://apify.epayco.co/login', array(
                     'headers' => array(
                         'Authorization' => 'Basic ' . base64_encode( $username . ':' . $password ),
                     ),
