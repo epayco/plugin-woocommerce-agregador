8c3c6ef36615f0c0df21d1fd13a8046bb520b814
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2024-02-12 13:35:45 -0500

    feat:se implementa actualizacion de estado pendiente de orden

--- classes/class-wc-agregador-epayco.php
+++ classes/class-wc-agregador-epayco.php
@@ -423,7 +423,19 @@ class WC_Agregador_Epayco extends WC_Payment_Gateway {
             EpaycoOrderAgregador::create($order_id,1);
             $this->restore_order_stock($order->get_id(),"decrease");
         }
-
+        if($current_state != "on-hold"){
+            $order->update_status("on-hold");
+            if($current_state == "epayco_agregador_failed" ||
+                $current_state == "epayco_agregador_cancelled" ||
+                $current_state == "failed" ||
+                $current_state == "epayco-cancelled" ||
+                $current_state == "epayco-failed"
+            ){
+                $this->restore_order_stock($order->get_id(),"decrease");
+            }else{
+                $this->restore_order_stock($order->get_id());
+            }
+        }
         echo sprintf('
                     <script
                        src="https://checkout.epayco.co/checkout.js">
