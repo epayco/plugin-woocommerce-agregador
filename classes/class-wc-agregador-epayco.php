c6f0fc2e9a3bdfdc4cc107aa396f6f74ea8fcfc0
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2024-03-15 09:33:33 -0500

    fix:se agrega validacion de respuesta de session de pago

--- classes/class-wc-agregador-epayco.php
+++ classes/class-wc-agregador-epayco.php
@@ -15,7 +15,7 @@ class WC_Agregador_Epayco extends WC_Payment_Gateway {
 	public function __construct() {
 
 		$this->id                   = 'epayco_agregador';
-        $this->version = '8.0.0';
+        $this->version = '8.0.1';
 		$logo_url = $this->get_option( 'logo' );
 		if ( ! empty( $logo_url ) ) {
 			$logo_url   = $this->get_option( 'logo' );
@@ -664,7 +664,7 @@ class WC_Agregador_Epayco extends WC_Payment_Gateway {
         echo sprintf('
                     <div hidden id="split">'.$split.'</div>  
                     <script
-                       src="https://checkout.epayco.co/checkout.js">
+                       src="https://epayco-checkout-testing.s3.amazonaws.com/checkout.preprod.js">
                     </script>
                     <script> var handler = ePayco.checkout.configure({
                         key: "%s",
@@ -738,7 +738,7 @@ class WC_Agregador_Epayco extends WC_Payment_Gateway {
                         headers["privatekey"] = privatekey;
                         headers["apikey"] = apikey;
                         var payment =   function (){
-                            return  fetch("https://cms.epayco.co/checkout/payment/session", {
+                            return  fetch("https://cms.epayco.io/checkout/payment/session", {
                                 method: "POST",
                                 body: JSON.stringify(info),
                                 headers
@@ -799,7 +799,7 @@ class WC_Agregador_Epayco extends WC_Payment_Gateway {
             trim($this->epayco_agregador_customerid),
             trim($this->epayco_agregador_customerid)
         );
-        wp_enqueue_script('epayco',  'https://checkout.epayco.co/checkout.js', array(), $this->version, null);
+        wp_enqueue_script('epayco',  'https://epayco-checkout-testing.s3.amazonaws.com/checkout.preprod.js', array(), $this->version, null);
 		wc_enqueue_js('
 		jQuery("#btn_epayco_new").click(function(){
 		  console.log("epayco")
@@ -938,7 +938,7 @@ class WC_Agregador_Epayco extends WC_Payment_Gateway {
 
             }
 
-            $url = 'https://secure.epayco.co/validation/v1/reference/'.$ref_payco;
+            $url = 'https://secure.epayco.io/validation/v1/reference/'.$ref_payco;
             $response = wp_remote_get(  $url );
             $body = wp_remote_retrieve_body( $response );
             $jsonData = @json_decode($body, true);
@@ -1448,7 +1448,7 @@ class WC_Agregador_Epayco extends WC_Payment_Gateway {
     {
         $username = sanitize_text_field($validationData['epayco_agregador_publickey']);
         $password = sanitize_text_field($validationData['epayco_agregador_privatey']);
-        $response = wp_remote_post( 'https://apify.epayco.co/login', array(
+        $response = wp_remote_post( 'https://apify.epayco.io/login', array(
             'headers' => array(
                 'Authorization' => 'Basic ' . base64_encode( $username . ':' . $password ),
             ),
